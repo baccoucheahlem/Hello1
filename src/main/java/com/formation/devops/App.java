1d2ef47d4892edad9911b8ae10acfaa6d8a2c5a6
baccoucheahlem <116307060+baccoucheahlem@users.noreply.github.com>
2023-11-28 18:34:38 +0100

    Update App.java

--- src/main/java/com/formation/devops/App.java
+++ src/main/java/com/formation/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Devops M1 !" );
+        System.out.println( "Bonjour Devops M1 !" );
     }
 }
